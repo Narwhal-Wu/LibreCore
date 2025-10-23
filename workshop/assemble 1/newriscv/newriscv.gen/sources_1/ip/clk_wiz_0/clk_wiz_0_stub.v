49ef83dd0f86633e30e416940f64a825e8c4de77
Narwhal-Wu <wuyiwei3256@qq.com>
2025-10-23 15:46:30 +0800

    整理文件增加存储读写测试

--- workshop/assemble 1/newriscv/newriscv.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+++ workshop/assemble 1/newriscv/newriscv.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -2,7 +2,7 @@
 // Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
-// Date        : Fri Oct 10 21:00:25 2025
+// Date        : Sat Sep 13 14:40:45 2025
 // Host        : lsh running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub -rename_top clk_wiz_0 -prefix
 //               clk_wiz_0_ clk_wiz_0_stub.v
